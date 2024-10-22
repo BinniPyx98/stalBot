1d1fc53482a3680f56d59810d3e09bad4e6f880f
Alex <aleks.kalinichenko989@gmail.com>
2024-10-22 23:39:39 +0300

    feat: update function create_screenshot and add def checkScreensDir

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # import keyboard
 # import time
+import time
+
 import cv2
 import pyautogui
 import os
@@ -14,10 +16,6 @@ import os
 #     keyboard.release('w')
 #     start = False
 
-def create_screenshot():
-    screenshot = pyautogui.screenshot()
-    screenshot.save(r'/screen.png')
-
 
 def getPath():
     path = os.getcwd()
@@ -26,7 +24,27 @@ def getPath():
 
 def createDir(dirName):
     path = getPath()
-    os.mkdir(path+'\\'+dirName)
+    os.mkdir(path + '\\' + dirName)
+
+
+def checkScreensDir():
+    dirs = os.listdir()
+    found = False
+    for i in range(len(dirs)):
+        if dirs[i] == 'screens':
+            found = True
+            break
+    if not found:
+        createDir('screens')
+
+
+def create_screenshot():
+    path = getPath()
+    fullPath= path+'/screens'
+    screenName = 'screen.png'
+    screenshot = pyautogui.screenshot()
+    screenshot.save(fullPath+'/'+screenName)
+
 
 def loading_displaying_saving():
     img = cv2.imread('proto.png', cv2.IMREAD_GRAYSCALE)
@@ -35,5 +53,10 @@ def loading_displaying_saving():
     cv2.imwrite('graygirl.jpg', img)
 
 
-createDir('screens')
-# create_screenshot()
+checkScreensDir()
+
+start = True
+while start:
+    create_screenshot()
+    time.sleep(10)
+
